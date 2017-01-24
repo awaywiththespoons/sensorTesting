886c970c769c85a3e33c3d4276beb56c573405ab
Becca Rose <hello@beccarose.co.uk>
2017-01-24 17:05:59 +0000

    did this work? (i.e. branch-mc-branch)

---
+++ Sensor Test/Assets/Scripts/SceneCtrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// making a way to navigate between scenes to explore more options with demo
+public class SceneCtrl : MonoBehaviour {
+
+	public void LoadScene(string sceneName){
+
+		SceneManager.LoadScene (sceneName);
+	
+	}
+
+}
