dcef8e2cd9f7a1232c31fe8a4339536241b35604
mark <ragzouken@gmail.com>
2017-02-04 18:49:56 +0000

    copy in new sensor tech

---
+++ Sensor Test/Assets/Scripts/SensorPlotter.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using Random = UnityEngine.Random;
+
+public class SensorPlotter : MonoBehaviour 
+{
+    private struct Data
+    {
+        public int id;
+        public Vector3 plot;
+        public Color color;
+    }
+
+    private Sensor sensor;
+    [SerializeField]
+    private SpriteRenderer plotTemplate;
+    private IndexedPool<SpriteRenderer> plots;
+    private List<Data> plotData = new List<Data>();
+
+    [SerializeField]
+    private SpriteRenderer sensorPlot;
+
+    [SerializeField]
+    private Text debugText;
+    [SerializeField]
+    private LineRenderer triangleRenderer;
+    [SerializeField]
+    private RectTransform shapeCanvas;
+
+    private int count;
+
+    public void Increment()
+    {
+        count += 1;
+
+        sensor.SetTraining(new Sensor.Token { id = count });
+    }
+
+    public void Reset()
+    {
+        count = 0;
+        plotData.Clear();
+        plots.SetActive(0);
+
+        sensor.Reset();
+        sensor.SetTraining(new Sensor.Token { id = count });
+    }
+
+    public void SetClassify(bool classify)
+    {
+        sensor.SetClassify();
+    }
+
+    private void Awake()
+    {
+        plots = new IndexedPool<SpriteRenderer>(plotTemplate);
+
+        Reset();
+    }
+
+    private static List<Vector2> SidesToTriangle(Vector2 position, Vector3 sides)
+    {
+        sides = -Triangle.CycleMinimiseX(-sides);
+
+        Vector2 a = Vector2.zero;
+        Vector2 b = a + Vector2.right * sides.x;
+
+        float angle = -Mathf.Acos((sides.x * sides.x + sides.y * sides.y - sides.z * sides.z) / (2 * sides.x * sides.y));
+
+        Vector2 c = a + new Vector2(Mathf.Cos(angle) * sides.y,
+                                    Mathf.Sin(angle) * sides.y);
+
+        Vector2 centroid = (a + b + c) / 3f;
+
+        return new List<Vector2> { a - centroid + position,
+                                   b - centroid + position,
+                                   c - centroid + position };
+    }
+
+    private static List<Vector2> CompleteTriangle(Vector2 a, Vector2 b, Vector3 sides)
+    {
+        float offset = Sensor.PolarAngle(b - a) * Mathf.Deg2Rad;
+        float angle = offset - Mathf.Acos((sides.x * sides.x + sides.y * sides.y - sides.z * sides.z) / (2 * sides.x * sides.y));
+
+        b = a + new Vector2(Mathf.Cos(offset) * sides.x,
+                                    Mathf.Sin(offset) * sides.x);
+        Vector2 c = a + new Vector2(Mathf.Cos(angle) * sides.y,
+                                    Mathf.Sin(angle) * sides.y);
+
+        return new List<Vector2> { a, b, c };
+    }
+
+    private void LateUpdate()
+    {
+        plotData.Clear();
+
+        foreach (var token in sensor.knowledge.tokens)
+        {
+            foreach (var data in token.training)
+            {
+                Color color = Color.HSVToRGB(token.id / 10f, 1, 1);
+
+                plotData.Add(new Data
+                {
+                    id = token.id,
+                    color = color,
+                    plot = data,
+                });
+
+                plotData.Add(new Data
+                {
+                    id = token.id,
+                    color = color,
+                    plot = Triangle.Cycle(data),
+                });
+
+                plotData.Add(new Data
+                {
+                    id = token.id,
+                    color = color,
+                    plot = Triangle.Cycle(Triangle.Cycle(data)),
+                });
+            }
+        }
+
+        float plotScale = 1;
+
+        if (plotData.Count > 0)
+        {
+            float max = plotData.Select(data => Mathf.Max(data.plot.x, data.plot.y, data.plot.z)).Max();
+            
+            if (max > 0)
+            {
+                plotScale = 1 / max;
+            }
+        }
+
+        plots.SetActive(plotData.Count);
+        plots.MapActive((i, plot) =>
+        {
+            plot.transform.localPosition = plotData[i].plot * plotScale;
+            plot.GetComponent<SpriteRenderer>().color = plotData[i].color;
+        });
+    }
+}
---
+++ Sensor Test/Assets/Scripts/Triangle.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using Random = UnityEngine.Random;
+
+public static class Triangle
+{
+    public static float Lineness(Vector3 vector)
+    {
+        float a = Mathf.Abs(1 - vector.x / (vector.y + vector.z));
+        float b = Mathf.Abs(1 - vector.y / (vector.x + vector.z));
+        float c = Mathf.Abs(1 - vector.z / (vector.x + vector.y));
+
+        return 1 - Mathf.Min(a, b, c) * 2;
+    }
+
+    public static bool IsLine(Vector3 vector)
+    {
+        return Lineness(vector) >= 0.95f;
+    }
+
+    public static float Compare(Vector3 a, Vector3 b)
+    {
+        return Vector3.Dot(b - a, b - a);
+    }
+
+    public static float CompareBestCycle(Vector3 a, Vector3 b)
+    {
+        return Compare(CycleToMatch(a, b), b);
+    }
+
+    public static Vector3 Cycle(Vector3 vector)
+    {
+        return new Vector3(vector.y, vector.z, vector.x);
+    }
+
+    public static Vector3 CycleToMatch(Vector3 vector, Vector3 other)
+    {
+        Vector3 a = vector;
+        Vector3 b = Cycle(a);
+        Vector3 c = Cycle(b);
+
+        float ad = Compare(a, other);
+        float bd = Compare(b, other);
+        float cd = Compare(c, other);
+
+        if (ad <= bd && ad <= cd)
+        {
+            return a;
+        }
+        else if (bd <= ad && bd <= cd)
+        {
+            return b;
+        }
+        else
+        {
+            return c;
+        }
+    }
+
+    public static Vector3 CycleToSide(Vector3 vector, float side)
+    {
+        Vector3 a = vector;
+        Vector3 b = Cycle(a);
+        Vector3 c = Cycle(b);
+
+        float ad = Mathf.Abs(side - a.x);
+        float bd = Mathf.Abs(side - b.x);
+        float cd = Mathf.Abs(side - c.x);
+
+        if (ad <= bd && ad <= cd)
+        {
+            return a;
+        }
+        else if (bd <= ad && bd <= cd)
+        {
+            return b;
+        }
+        else
+        {
+            return c;
+        }
+    }
+
+    public static Vector3 CycleMinimiseX(Vector3 vector)
+    {
+        Vector3 a = vector;
+        Vector3 b = Cycle(a);
+        Vector3 c = Cycle(b);
+
+        if (a.x <= b.x && a.x <= c.x)
+        {
+            return a;
+        }
+        else if (b.x <= a.x && b.x <= c.x)
+        {
+            return b;
+        }
+        else
+        {
+            return c;
+        }
+    }
+
+    private static void Sort(ref float a, ref float b)
+    {
+        if (a > b)
+        {
+            float c = a;
+            a = b;
+            b = c;
+        }
+    }
+
+    public static Vector3 SortSides(Vector3 vector)
+    {
+        Vector3 copy = vector;
+
+        Sort(ref copy.x, ref copy.y);
+        Sort(ref copy.y, ref copy.z);
+        Sort(ref copy.x, ref copy.y);
+
+        return copy;
+    }
+
+    public static Vector3 Mean(IList<Vector3> triangles)
+    {
+        Vector3 sum = Vector3.zero;
+
+        for (int i = 0; i < triangles.Count; ++i)
+        {
+            sum += triangles[i];
+        }
+
+        return sum / triangles.Count;
+    }
+}
