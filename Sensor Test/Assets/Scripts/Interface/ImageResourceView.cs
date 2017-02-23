70c011dbb60fe03054c07fe4972ae7248d6dbef0
mark <ragzouken@gmail.com>
2017-02-23 17:25:21 +0000

    add options to disable ghosts/touches

---
+++ Sensor Test/Assets/Scripts/Interface/ImageResourceView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using Random = UnityEngine.Random;
+
+public class ImageResourceView : InstanceView<TestImageBrowser.ImageResource> 
+{
+    [SerializeField]
+    private Text nameText;
+    [SerializeField]
+    private Image image;
+
+    protected override void Configure()
+    {
+        base.Configure();
+
+        nameText.text = config.name;
+        image.sprite = config.sprite;
+    }
+}
